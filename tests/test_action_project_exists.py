2131c549a52090d5764f072d167bab8d8b6d2b3f
Konrad Kleine <kkleine@redhat.com>
2022-10-11 11:39:19 +0200

    test: add project exists action test

---
+++ tests/test_action_project_exists.py
@@ -0,0 +1,25 @@
+""" Tests for llvm_snapshot_builder """
+
+import os
+import unittest
+import uuid
+from llvm_snapshot_builder.actions.project_exists import CoprActionProjectExists
+from llvm_snapshot_builder.copr_project_ref import CoprProjectRef
+
+
+class TestCoprActionProjectExists(unittest.TestCase):
+    """ Testcases for the CoprActionProjectExists class. """
+
+    def test_not_existing_project(self):
+        """ Test that a non-existing project is detected as such. """
+        action = CoprActionProjectExists(f"@copr/{uuid.uuid4()}")
+        self.assertFalse(action.run())
+
+    def test_existing_project(self):
+        """ Test that @copr/copr project can be found. """
+        action = CoprActionProjectExists(f"@copr/copr")
+        self.assertTrue(action.run())
+
+
+if __name__ == '__main__':
+    unittest.main()
